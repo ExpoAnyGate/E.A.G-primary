aea77483c355cb262b9b7f603c85860fe8718913
HarryTsai <sleepingturtle990866@gmail.com>
2025-03-01 00:32:53 +0800

    first push

--- src/layouts/Frontlayout.jsx
+++ src/layouts/Frontlayout.jsx
@@ -1,36 +1,49 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import LoginStatus from "../components/LoginStatus";
-import React, { useState, useEffect } from "react";
-
+import React from "react";
 
 const routes = [
-  { path: "/", name: "首頁" },
-  { path: "/search", name: "搜尋" },
-  { path: "/demo", name: "展覽" },
-  { path: "/donate", name: "乾爹抖內" },
-  { path: "/wish", name: "展覽選擇泉" },
-  { path: "/goods", name: "周邊商品" },
-  { path: "/spotlight", name: "展覽聚光燈" },
+	{ path: "/", name: "首頁" },
+	{ path: "/search", name: "搜尋" },
+	{ path: "/wish", name: "展覽選擇泉" },
+	{ path: "/demo", name: "展覽投票區" },
+	{ path: "/goods", name: "周邊商品頁" },
+	{ path: "/spotlight", name: "藝展聚光燈" },
+	{ path: "/donate", name: "乾爹抖內" },
 ];
 
-export default function Frontlayout({ }) {
-    
-  return (
-    <>
-      <nav class="bg-white position-breakpoint">
-        <div className="container py-6 navbar-layout">
-            <div class="d-flex justify-content-between align-items-center">
-                <a href="index.html">
-                    <img src="Nav/logo-Light.png" alt="logo" />
-                </a>
+const NavbarItem = ["/", "/goods", "/spotlight", "/wish"];
+export default function FrontLayout() {
+	return (
+		<>
+			<nav className='bg-white position-breakpoint'>
+				<div className='container py-6 navbar-layout'>
+					<div className='d-flex justify-content-between align-items-center'>
+						{routes
+							.filter((route) => NavbarItem.includes(route.path))
+							.map((route) => {
+								return (
+									<li key={route.path}>
+										<NavLink to={route.path}>
+											{route.path === "/" ? (
+												<img
+													src='src/assets/images/Nav/logo-Light.png'
+													alt='logo'
+												/>
+											) : (
+												route.name
+											)}
+										</NavLink>
+									</li>
+								);
+							})}
 
-                <LoginStatus />
+						<LoginStatus />
+					</div>
+				</div>
+			</nav>
 
-            </div>
-        </div>
-      </nav>
-      
-      <Outlet></Outlet>
-    </>
-  );
+			<Outlet></Outlet>
+		</>
+	);
 }

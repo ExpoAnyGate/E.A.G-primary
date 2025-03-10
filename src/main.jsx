b5fc554dd65bce266f086cc57b6c99718196fec7
HarryTsai <sleepingturtle990866@gmail.com>
2025-03-11 00:10:15 +0800

    login test

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
-import "./assets/scss/all.scss"
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js'
-
-
-
-// import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js";
+import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<AuthProvider>
+			<RouterProvider router={router} />
+		</AuthProvider>
 	</StrictMode>
 );

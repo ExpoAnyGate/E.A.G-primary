f29122d120fa0aafb705f480e708b6ca2791f010
HarryTsai <sleepingturtle990866@gmail.com>
2025-03-11 00:26:57 +0800

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,25 @@
 import { StrictMode } from "react";
+import { AuthProvider } from "./context/AuthContext";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { RouterProvider } from "react-router-dom";
-import router from "./router";
+import { store, persistor } from "./hook/usePersistStore"; // 引入 store 和 persistor
+
+import router from "./router"; // 假设你有定义好的 router 配置
+
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js";
-import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<AuthProvider>
-			<RouterProvider router={router} />
-		</AuthProvider>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<AuthProvider>
+					<RouterProvider router={router} />
+				</AuthProvider>
+			</PersistGate>
+		</Provider>
 	</StrictMode>
 );
